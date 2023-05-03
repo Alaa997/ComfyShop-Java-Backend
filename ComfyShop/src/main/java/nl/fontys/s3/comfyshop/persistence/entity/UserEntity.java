f80f66e96fb0b45314e65785a0c6d0d2d0b4c1aa
Alaa997 <alaa.tarakji997@gmail.com>
2023-05-03 16:44:20 +0200

    sonarqube added

---
+++ ComfyShop/src/main/java/nl/fontys/s3/comfyshop/persistence/entity/UserEntity.java
@@ -0,0 +1,19 @@
+package nl.fontys.s3.comfyshop.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+
+}
